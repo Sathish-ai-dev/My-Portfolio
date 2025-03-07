692cdb1996f8f0b6e765b828b9e9e1c5274c744f
CharanSuggala26 <suggalasaicharan789@gmail.com>
2025-03-07 17:04:53 +0530

    Fixed Skills page

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,4 +1,7 @@
-// const Experience = () => {
+
+
+
+// const Skills = () => {
 //   const techs = [
 //     { id: 1, title: "React.js", style: "shadow-blue-500" },
 //     { id: 2, title: "JavaScript", style: "shadow-yellow-500" },
@@ -21,17 +24,20 @@
 //     <div name="experience" className="bg-gradient-to-b from-gray-800 to-black w-full h-auto py-8">
 //       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full text-white">
 //         <div>
-//           <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">Skills</p>
+//           <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline mt-5">Skills</p>
 //           <p className="py-6">These are the technologies I've worked with</p>
 //         </div>
 
-//         <div className="w-full grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-8 text-center px-4 sm:px-0">
+//         {/* Parent container with perspective */}
+//         <div className="perspective-1500px w-full grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-8 text-center px-4 sm:px-0">
 //           {techs.map(({ id, title, style }) => (
 //             <div
 //               key={id}
-//               className={`shadow-md hover:scale-105 duration-500 py-4 rounded-lg ${style}`}
+//               className={`group relative shadow-md py-4 rounded-lg ${style} transform transition-all duration-500 hover:scale-110 hover:rotate-x-6 hover:rotate-y-6`}
 //             >
-//               <p className="mt-4">{title}</p>
+//               <div className="transform-gpu group-hover:scale-105 transition-all duration-500">
+//                 <p className="mt-4">{title}</p>
+//               </div>
 //             </div>
 //           ))}
 //         </div>
@@ -40,7 +46,7 @@
 //   );
 // };
 
-// export default Experience;
+// export default Skills;
 
 
 const Skills = () => {
@@ -63,10 +69,10 @@ const Skills = () => {
   ];
 
   return (
-    <div name="experience" className="bg-gradient-to-b from-gray-800 to-black w-full h-auto py-8 ">
+    <div name="experience" className="bg-gradient-to-b from-gray-800 to-black w-full h-auto py-8">
       <div className="max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full text-white">
         <div>
-          <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">Skills</p>
+          <p className="text-4xl font-bold border-b-4 border-gray-500 p-5 inline">Skills</p>
           <p className="py-6">These are the technologies I've worked with</p>
         </div>
 
